70aa9dd0b606ae9bca59b2697f45614e10f67b68
BloodCharry <vladvorobei1994@gmail.com>
2024-11-17 19:41:07 +0300

    добавил в админ панель отображение моделей

--- backend/src/profiles/admin.py
+++ backend/src/profiles/admin.py
@@ -1,14 +1,14 @@
 from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import UserNet
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import UserNet, Patient, Doctor
 
 
-class UserNetAdmin(UserAdmin):
-    list_display = ('username', 'first_name', 'last_name', 'role', 'email', 'phoneNumber', 'is_staff')
+class UserNetAdmin(BaseUserAdmin):
+    list_display = ('username', 'email',)
     fieldsets = (
         (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (_("Personal info"), {"fields": ("email",)}),
         (
             _("Permissions"),
             {
@@ -22,9 +22,27 @@ class UserNetAdmin(UserAdmin):
             },
         ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
-        (_("Additional info"), {"fields": ("phoneNumber", "avatar", "role")}),
+    )
+
+
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('user', 'verified', 'first_name', 'last_name', 'role')
+    list_editable = ['verified']
+    fieldsets = (
+        (None, {"fields": ("user",)}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "phone", "avatar", "gender", "birthday", "role")}),
+        (_("Permissions"), {"fields": ("verified",)}),
+    )
+
+
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'specialization', 'education', 'certification', 'experience')
+    fieldsets = (
+        (None, {"fields": ("user",)}),
+        (_("Professional info"), {"fields": ("specialization", "education", "certification", "bio", "experience")}),
     )
 
 
 admin.site.register(UserNet, UserNetAdmin)
-
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(Doctor, DoctorAdmin)
